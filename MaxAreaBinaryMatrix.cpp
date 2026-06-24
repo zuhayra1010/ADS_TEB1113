1640d33f20ca19a10e129e0dc36e54b4cb542696
ayraarinnn <zuhayra1010@gmail.com>
2026-06-25 00:53:53 +0800

    Update MaxAreaBinaryMatrix.cpp

--- MaxAreaBinaryMatrix.cpp
+++ MaxAreaBinaryMatrix.cpp
@@ -3,46 +3,54 @@
 #include <algorithm>
 using namespace std;
 
-int maxArea(vector<vector<int>> &mat) {
-    int n = mat.size(); //row
-    int m = mat[0].size(); //column
+int maxArea(vector<vector<int>> &mat)
+{
+    int row = mat.size();
+    int col = mat[0].size();
 
-    // ending at position (i,j)
-    vector<vector<int>> memo(n , vector<int>(m, 0));
-    int ans = 0;
+    vector<vector<int>> memo(row, vector<int>(col, 0));
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (mat[i][j] == 0) {
+    int maxA = 0;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (mat[i][j] == 0)
                 continue;
-            }
 
-            // computer width of 1's at (i,j)
-            memo[i][j] = (j == 0) ? 1 : memo[i][j - 1] + 1;
+            if (j == 0)
+                memo[i][j] = 1;
+            else
+                memo[i][j] = memo[i][j - 1] + 1;
 
             int width = memo[i][j];
 
-            // traverse upwards row by row
-            // update minimum width 
-            for (int k = i; k >= 0; k--) {
+            for (int k = i; k >= 0; k--)
+            {
                 width = min(width, memo[k][j]);
+
                 int area = width * (i - k + 1);
-                ans = max(ans, area);
+
+                maxA = max(maxA, area);
             }
         }
     }
-    return ans;
+
+    return maxA;
 }
 
-int main() {
-    vector<vector<int>> mat = {
+int main()
+{
+    vector<vector<int>> mat =
+    {
         {0, 1, 1, 0},
         {1, 1, 1, 1},
         {1, 1, 1, 0},
         {1, 1, 0, 0}
     };
 
-    cout << maxArea(mat) << endl; // 6
+    cout << "Area = " << maxArea(mat) << endl;
 
     return 0;
 }
